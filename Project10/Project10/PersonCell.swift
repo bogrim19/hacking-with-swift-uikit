095a7dad5ac39b6a7d1083849bee1f0ea6997a3d
Bogdan Barbus <47646017+bogrim19@users.noreply.github.com>
2023-04-25 15:19:22 +0300

    Added Project10

---
+++ Project10/Project10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Bogrim on 31.03.2023.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var PersonName: UILabel!
+    @IBOutlet var PersonImage: UIImageView!
+}
